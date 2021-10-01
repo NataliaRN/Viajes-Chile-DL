ea934ffe6dfb2fbca663cd365d862232e03ad696
Natalia Romero <83964278+NataliaRN@users.noreply.github.com>
2021-10-01 02:39:53 -0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    console.log("ready");
+    
+    
+    $("a").click(function (event) {
+      event.preventDefault();
+  
+      var gato = this.hash;
+  
+      $("html").animate(
+        {
+          scrollTop: $(gato).offset().top - 76,
+        },
+        800
+      );
+    });
+  
+    $(window).scroll(function () {
+      var scroll = $(window).scrollTop();
+      if (scroll < 300) {
+        $(".fixed-top").css("background", "transparent");
+      } else {
+        $(".fixed-top").css("background", "#15aabf");
+      }
+    });
+  });
+  
+  $('[data-toggle="tooltip"]').tooltip();
+  
